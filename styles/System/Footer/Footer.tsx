a11bb6296d4eac029ca13e7407d7bde77c6c93c2
heart <66947260+bampudding@users.noreply.github.com>
2023-09-25 15:30:01 +0900

    Update Footer.tsx

--- styles/System/Footer/Footer.tsx
+++ styles/System/Footer/Footer.tsx
@@ -1,27 +1,36 @@
 import { ThemeButtons } from "@/modules/SystemFunction/Theme"
+import { AnimatePresence, motion } from "framer-motion"
 import styles from "./Footer.module.css"
 
 const Footer = () => {
     return (
-        <footer className={styles.Group}>
-            <div className={styles.Container}>
-                <div className={styles.LeftContent}>
-                    <div className={styles.Promotion}>
-                        <p>Build</p>
-                        <h5>IT_Tech_Talks</h5>
-                        <h5>Community</h5>
-                        <h5>Content</h5>
+        <AnimatePresence>
+            <motion.footer
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: .2 }}
+                className={styles.Group}
+            >
+                <div className={styles.Container}>
+                    <div className={styles.LeftContent}>
+                        <div className={styles.Promotion}>
+                            <p>Build</p>
+                            <h5>IT_Tech_Talks</h5>
+                            <h5>Community</h5>
+                            <h5>Content</h5>
+                        </div>
+                        <div className={styles.Promotion}>
+                            <p>Make</p>
+                            <h6>{`<DEVELOID/>`}</h6>
+                        </div>
                     </div>
-                    <div className={styles.Promotion}>
-                        <p>Make</p>
-                        <h6>{`<DEVELOID/>`}</h6>
+                    <div className={styles.RightContent}>
+                        <ThemeButtons/>
                     </div>
                 </div>
-                <div className={styles.RightContent}>
-                    <ThemeButtons/>
-                </div>
-            </div>
-        </footer>
+            </motion.footer>
+        </AnimatePresence>
     )
 }
 
